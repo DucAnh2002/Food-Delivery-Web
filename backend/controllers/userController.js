d1396da1a6e78e98d3ca501e3584d47dc32cbc33
DucAnh2002 <haducanh02012002@gmail.com>
2025-04-19 22:53:24 +0700

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import bcryptjs from "bcryptjs";
 import validator from "validator";
 
 // login user
@@ -9,10 +9,10 @@ const loginUser = async (req, res) => {
   try {
     const user = await userModel.findOne({ email });
     if (!user) {
-      return res.json({ success: false, message: "User Doesnt exist" });
+      return res.json({ success: false, message: "User doesn't exist" });
     }
-    const isMatch = await bcrypt.compare(password, user.password);
 
+    const isMatch = await bcryptjs.compare(password, user.password);
     if (!isMatch) {
       return res.json({ success: false, message: "Invalid credentials" });
     }
@@ -30,37 +30,34 @@ const createToken = (id) => {
 };
 
 // register user
-
 const registerUser = async (req, res) => {
   const { name, password, email } = req.body;
   try {
-    // checking is user already exists
     const exists = await userModel.findOne({ email });
     if (exists) {
       return res.json({ success: false, message: "User already exists" });
     }
 
-    // validating email format @ Strong password
     if (!validator.isEmail(email)) {
       return res.json({
         success: false,
         message: "Please enter a valid email!",
       });
     }
+
     if (password.length < 8) {
       return res.json({
         success: false,
-        message: "{Please enter a strong password!}",
+        message: "Please enter a strong password!",
       });
     }
 
-    // hashing user password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
 
     const newUser = new userModel({
-      name: name,
-      email: email,
+      name,
+      email,
       password: hashedPassword,
     });
 
