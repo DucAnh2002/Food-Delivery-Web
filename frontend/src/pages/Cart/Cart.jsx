598a02ca339647a509f3e502acd739e2f62f7db1
DucAnh2002 <haducanh02012002@gmail.com>
2025-07-21 02:01:01 +0700

    fix cart

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -22,26 +22,25 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {food_list.map((item, index) => {
-          if (cartItems[item._id] > 0) {
-            return (
-              <div>
-                <div className="cart-items-title cart-items-item">
-                  <img src={url + "/images/" + item.image} alt="" />
-                  <p>{item.name}</p>
-                  <p>${item.price}</p>
-                  <p>{cartItems[item._id]}</p>
-                  <p>${item.price * cartItems[item._id]}</p>
-                  <p onClick={() => removeFromCart(item._id)} className="cross">
-                    X
-                  </p>
-                </div>
-                <hr />
+        {food_list
+          ?.filter((item) => item && cartItems[item._id] > 0)
+          .map((item, index) => (
+            <div key={item._id || index}>
+              <div className="cart-items-title cart-items-item">
+                <img src={url + "/images/" + item.image} alt={item.name} />
+                <p>{item.name}</p>
+                <p>${item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>${(item.price * cartItems[item._id]).toFixed(2)}</p>
+                <p onClick={() => removeFromCart(item._id)} className="cross">
+                  X
+                </p>
               </div>
-            );
-          }
-        })}
+              <hr />
+            </div>
+          ))}
       </div>
+
       <div className="cart-bottom">
         <div className="cart-total">
           <h2>Cart Total</h2>
@@ -59,7 +58,10 @@ const Cart = () => {
             <div className="cart-total-details">
               <p>Total</p>
               <p>
-                ${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+                $
+                {getTotalCartAmount() === 0
+                  ? 0
+                  : (getTotalCartAmount() + 2).toFixed(2)}
               </p>
             </div>
           </div>
@@ -67,6 +69,7 @@ const Cart = () => {
             PROCEED TO CHECKOUT
           </button>
         </div>
+
         <div className="cart-promocode">
           <div>
             <p>If you have a promo code, Enter it here</p>
